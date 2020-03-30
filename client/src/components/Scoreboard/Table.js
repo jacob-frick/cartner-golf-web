81e7171191a2ec67852ac9d6ad85155d252737f1
sulakshana borsune <sulakshana.borsune@gmail.com>
2020-03-30 00:33:56 -0700

    changes in table rows and column names

--- client/src/components/Scoreboard/Table.js
+++ client/src/components/Scoreboard/Table.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -15,22 +15,56 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import "./Table.css";
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650,
+    minWidth: 700,
   },
 });
 
-function createData(player, strokes, hole, putts, par, total) {
-  return { player, strokes, hole, putts, par, total };
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+}))(TableRow);
+
+
+function createData(hole, one, two, three, four, five ) {
+  return { hole, one, two, three, four, five };
 }
 
 const rows = [
-  createData('Player1'),
-  createData('Player2'),
+  createData('Black', 1,2,3,4,5),
+  createData('blackhandicap'),
+  createData('blue'),
+  createData('bluehandicap'),
+  createData('white'),
+  createData('whitehandicap'),
+  createData('combo'),
+  createData('combohandicap'),
+  createData('menshandicap'),
+  createData('menspar'),
+  createData('womenspar'),
+  createData('womenshandicap'),
 ];
+
+const columns = [
+  createData()
+]
 
 export default function SimpleTable() {
   const classes = useStyles();
@@ -42,42 +76,107 @@ export default function SimpleTable() {
 
 
   return (
-    <TableContainer class= "table" component={Paper}>
+    <TableContainer  class = "table" component={Paper}>
               <Typography  class = "typo" variant="h1" id="tableTitle">
           Scoreboard
         </Typography>
 
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
-          <TableRow>
-            <TableCell>Player</TableCell>
-            <TableCell align="center">Strokes</TableCell>
-            <TableCell align="center">Hole</TableCell>
-            <TableCell align="center">Putts</TableCell>
-            <TableCell align="center">Par</TableCell>
+          <TableRow class = "hole">
+            <TableCell  >Hole</TableCell>
+            <TableCell align="center">1</TableCell>
+            <TableCell align="center">2</TableCell>
+            <TableCell align="center">3</TableCell>
+            <TableCell align="center">4</TableCell>
+            <TableCell align="center">5</TableCell>
+            <TableCell align="center">6</TableCell>
+            <TableCell align="center">7</TableCell>
+            <TableCell align="center">8</TableCell>
+            <TableCell align="center">9</TableCell>
+            <TableCell align="center">Out</TableCell>
+            <TableCell align="center">10</TableCell>
+            <TableCell align="center">11</TableCell>
+            <TableCell align="center">12</TableCell>
+            <TableCell align="center">13</TableCell>
+            <TableCell align="center">14</TableCell>
+            <TableCell align="center">15</TableCell>
+            <TableCell align="center">16</TableCell>
+            <TableCell align="center">17</TableCell>
+            <TableCell align="center">18</TableCell>
+            <TableCell align="center">In</TableCell>
             <TableCell align="center">Total</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
-          {rows.map(row => (
+        
+        <TableBody class = "blue">
+        {rows.map((row) => (
             <TableRow key={row.name}>
               <TableCell component="th" scope="row">
-              <TextField id="filled-basic" label="Player" variant="filled" />
+                {row.name}
               </TableCell>
               <TableCell align="center" >
-              <TextField id="filled-basic" label="Strokes" variant="filled" />
+              <TextField id="standard-size-normal"  />
                 </TableCell>
               <TableCell align="center">
-              <TextField id="filled-basic" label="Hole" variant="filled" />
+              <TextField id="standard-size-normal"  />
                 </TableCell>
               <TableCell align="center">
-              <TextField id="filled-basic" label="Putts" variant="filled" />
+              <TextField id="standard-size-normal"  />
                 </TableCell>
               <TableCell align="center">
-              <TextField id="filled-basic" label="Par" variant="filled" />
+              <TextField id="standard-size-normal"  />
                 </TableCell>
               <TableCell align="center">
-              <TextField id="filled-basic" label="Total" variant="filled" />
+              <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
+                </TableCell>
+                <TableCell align="center">
+                <TextField id="standard-size-normal"  />
                 </TableCell>
 
             </TableRow>
