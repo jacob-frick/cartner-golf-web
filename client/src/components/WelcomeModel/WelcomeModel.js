0903f136c4ccc13c191ceb1256a504dca6f768b8
Julian <Van Ost>
2020-03-30 14:34:15 -0700

    Adds slide-in from left dialog instead of welcom  modal. Can extrapolate further later. Moved styles to separate css file. Considering changing file names to 'WelcomeDialog'

--- client/src/components/WelcomeModel/WelcomeModel.js
+++ client/src/components/WelcomeModel/WelcomeModel.js
@@ -1,62 +1,58 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Modal from '@material-ui/core/Modal'
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10
-}
+import WelcomeModalStyles from "./styles";
+// import Modal from '@material-ui/core/Modal'
 
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import Slide from '@material-ui/core/Slide'
 
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  }
-}
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: 'absolute',
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}))
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="right" ref={ref} {...props} />
+})
 
-export default function SimpleModal() {
-  const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
-  const [open, setOpen] = React.useState(true);
-
+export default function WelcomModel() {
   const handleClose = () => {
-    setOpen(false);
+    setOpen(false)
   }
-
-  const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <h2 id="simple-modal-title">Welcome!</h2>
-      <p id="simple-modal-description">
-        This is a welcmoe model
-      </p>
-    </div>
-  )
+  const classes = WelcomeModalStyles()
+  // getWelcomeModalStyles is not a pure function, we roll the style only on the first render
+  const [open, setOpen] = React.useState(true)
 
   return (
     <div>
-      <Modal
+      <Dialog
         open={open}
+        TransitionComponent={Transition}
+        keepMounted
         onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+        className={classes.dialogStyle}
       >
-        {body}
-      </Modal>
+        <DialogContent>
+          <div>
+            <h2 id="simple-modal-title">Welcome to Cartner-Golf!</h2>
+            <p id="simple-modal-description">
+              With this application you can keep track of your scores, stats, handicaps, and add friends to matches in order to use the app to track current games.
+          </p>
+            <p>
+              Use the navbar on the left to explore the various other features of this app.
+          </p>
+            <h2 id="simple-modal-title">Happy Golfing!</h2>
+          </div>
+
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Okay!
+          </Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
