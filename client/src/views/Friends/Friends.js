bb3c7045547fc72526d02c2c878fd4eb1126705d
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-30 22:52:33 -0700

    update

--- client/src/views/Friends/Friends.js
+++ client/src/views/Friends/Friends.js
@@ -1,38 +1,13 @@
 import React from 'react'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import Container from '@material-ui/core/Container'
-import Grid from '@material-ui/core/Grid'
-import DrawerContext from '../../utils/DrawerContext'
-import Appbar from '../../components/Appbar'
-import dashboardStyles from './../Dashboard/styles.js'
+import OuterNavbar from './../../components/OuterNavbar'
 
 
 const Friends = () => {
-    const classes = dashboardStyles()
-    const [open, setOpen] = React.useState({ open: false })
-    open.handleDrawerOpen = () => {
-      setOpen({ open: true });
-    }
-    open.handleDrawerClose = () => {
-      setOpen({ open: false });
-    }
 
     return (
-        <DrawerContext.Provider value={open}>
-          <div className={classes.root}>
-            <CssBaseline />
-            <Appbar />
-            <main className={classes.content}>
-              <div className={classes.appBarSpacer} />
-              <Container maxWidth="lg" className={classes.container}>
-                <Grid container spacing={3}>
-                  {/* Page Info goes here */}
-                  <h1>Friends</h1>
-                </Grid>
-              </Container>
-            </main>
-          </div>
-        </DrawerContext.Provider>
+        <OuterNavbar>
+            <h1>Friends</h1>
+        </OuterNavbar>
       )
 }
 export default Friends
