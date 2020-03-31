f15fa64ca19789fef6c5c80f000d77619c27fa0d
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-31 09:56:56 -0700

    added user utils

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -9,165 +9,107 @@ import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import GolfCourseIcon from '@material-ui/icons/GolfCourse';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import HomeContext from './../../utils/HomeContext'
-import axios from '../../config/axiosConfig';
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  loginStyle: {
-    backgroundColor: 'white',
-    maxHeight: '50%',
-    marginTop: '50%',
-    marginBottom: '50%',
-    paddingBottom: '2rem',
-    paddingRight: '2rem',
-    paddingLeft: '2rem',
-    borderRadius: '.8rem',
-    paddingTop: '1rem',
-    display: 'inline-table'
-  },
-  avatar: {
-    // margin: theme.spacing(1),
-    backgroundColor: theme.palette.primary.main,
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginTop: '2rem'
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  backgroundImage: {
-    display: 'flex',
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    backgroundImage: `url(${"../assets/images/backgroundImage.jpg"})`,
-  },
-  textCenter: {
-    textAlign: 'center',
-    marginTop: '1rem'
-  },
-  header: {
-    textAlign: 'center',
-    fontFamily: 'Open Sans, sans- serif'
-  },
-  onLinkHover: {
-    '&: hover': {
-      cursor: 'pointer',
-    }
-  }
-}));
-
+import User from './../../utils/User'
+import logInStyles from './styles.js'
 export default function Login() {
-  const classes = useStyles();
+  const classes = logInStyles()
   const { setPageCreateAccount, setPageDashboard } = useContext(HomeContext)
-  const [user, setUser] = useState({email: '', password: '', emailCheck: false, passwordCheck: false, emailErrorMessage: '', passwordErrorMessage: ''})
+  const [user, setUser] = useState({ email: '', password: '', emailCheck: false, passwordCheck: false, emailErrorMessage: '', passwordErrorMessage: '' })
   const formChange = (event) => {
     setUser({ ...user, [event.target.name]: event.target.value })
   }
   const didSubmit = (event) => {
     event.preventDefault()
     //reset error messages
-    axios.post('/api/users/login',{
-      username: user.email,
-      password: user.password
-    }).then( ({data}) => {
-      if(data.message) {
-        // console.log(data.message)
-        if(data.message==='Password Incorrect'){
-          setUser({...user, passwordCheck: true, passwordErrorMessage: 'Incorrect Password', emailCheck: false, emailErrorMessage: '' })
+    User.login({ username: user.email, password: user.password })
+      .then(({ data }) => {
+        if (data.message) {
+          // console.log(data.message)
+          if (data.message === 'Password Incorrect') {
+            setUser({ ...user, passwordCheck: true, passwordErrorMessage: 'Incorrect Password', emailCheck: false, emailErrorMessage: '' })
+          }
+          else {
+            setUser({ ...user, emailCheck: true, emailErrorMessage: 'Email Does Not Exist', passwordCheck: false, passwordErrorMessage: '' })
+          }
+        } else {
+          localStorage.setItem('jwt', data.token)
+          setPageDashboard()
         }
-        else{
-          setUser({ ...user, emailCheck: true, emailErrorMessage: 'Email Does Not Exist', passwordCheck: false, passwordErrorMessage: '' })
-        }
-      }else {
-        localStorage.setItem('jwt', data.token)
-        setPageDashboard()
-      }
-    })
+      })
   }
   return (
     <div className={classes.backgroundImage}>
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
         <div className={`${classes.paper} ${classes.loginStyle}`}>
-        <Typography className={classes.header} component="h1" variant="h3">
+          <Typography className={classes.header} component="h1" variant="h3">
             Cartner-Golf
         </Typography>
-        <Avatar className={classes.avatar}>
-          <GolfCourseIcon />
-        </Avatar>
-        <Typography className = {classes.textCenter}component="h1" variant="h5">
-          Login
+          <Avatar className={classes.avatar}>
+            <GolfCourseIcon />
+          </Avatar>
+          <Typography className={classes.textCenter} component="h1" variant="h5">
+            Login
         </Typography>
-        <form className={classes.form} noValidate onSubmit={didSubmit}>
-          <TextField
-            error = {user.emailCheck}
-            helperText= {user.emailErrorMessage}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            onChange={(event) => {formChange(event)}}
-          />
-          <TextField
-            error= {user.passwordCheck}
-            helperText = {user.passwordErrorMessage}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            onChange={(event) => {formChange(event)}}
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
+          <form className={classes.form} noValidate onSubmit={didSubmit}>
+            <TextField
+              error={user.emailCheck}
+              helperText={user.emailErrorMessage}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={(event) => { formChange(event) }}
+            />
+            <TextField
+              error={user.passwordCheck}
+              helperText={user.passwordErrorMessage}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              onChange={(event) => { formChange(event) }}
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link variant="body2">
-                Forgot password?
+            <Grid container>
+              <Grid item xs>
+                <Link variant="body2">
+                  Forgot password?
               </Link>
+              </Grid>
+              <Grid item>
+                <Link className={classes.onLinkHover} onClick={setPageCreateAccount} variant="body2">
+                  {"No Account? Sign Up"}
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link className = {classes.onLinkHover}onClick={setPageCreateAccount} variant="body2">
-                {"No Account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
+          </form>
+        </div>
+      </Container>
     </div>
   )
 }
---
+++ client/src/views/Login/styles.js
@@ -0,0 +1,56 @@
+import { makeStyles } from '@material-ui/core/styles'
+const logInStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    loginStyle: {
+        backgroundColor: 'white',
+        maxHeight: '50%',
+        marginTop: '50%',
+        marginBottom: '50%',
+        paddingBottom: '2rem',
+        paddingRight: '2rem',
+        paddingLeft: '2rem',
+        borderRadius: '.8rem',
+        paddingTop: '1rem',
+        display: 'inline-table'
+    },
+    avatar: {
+        // margin: theme.spacing(1),
+        backgroundColor: theme.palette.primary.main,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: '2rem'
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    backgroundImage: {
+        display: 'flex',
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        backgroundImage: `url(${"../assets/images/backgroundImage.jpg"})`,
+    },
+    textCenter: {
+        textAlign: 'center',
+        marginTop: '1rem'
+    },
+    header: {
+        textAlign: 'center',
+        fontFamily: 'Open Sans, sans- serif'
+    },
+    onLinkHover: {
+        '&: hover': {
+            cursor: 'pointer',
+        }
+    }
+}))
+export default logInStyles
