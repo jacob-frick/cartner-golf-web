04591bd82a0c1f94eb05f4b1f51f8477947fc77f
richard-dinh <richarddinh@gmail.com>
2020-04-01 10:47:08 -0700

    created columns in the friends view

---
+++ client/src/components/FriendsDisplay/FriendsDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+const FriendsDisplay = () => {
+  return(
+
+  )
+}
+
+export default FriendsDisplay
--- client/src/views/Friends/Friends.js
+++ client/src/views/Friends/Friends.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import OuterNavbar from './../../components/OuterNavbar'
 import SearchBar from './../../components/SearchBar'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
 
 const Friends = () => {
 
     return (
         <OuterNavbar>
             <SearchBar />
+            <Container>
+                <Grid container spacing={1}>
+                    <Grid item md={6} xs={12}>
+                        <h1>Friends List</h1>
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+                        <Container>
+                            <Grid item xs={12}>
+                                <h1>Pending Request</h1>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <h1>Sent Requests</h1>
+                            </Grid>
+                        </Container>
+                    </Grid>
+                </Grid>
+            </Container>
         </OuterNavbar>
       )
 }
