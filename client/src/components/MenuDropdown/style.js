9242ebfb17d70769dd40ee9a98aae99e2e11796d
Julian <Van Ost>
2020-04-03 19:32:30 -0700

    adds theme.json and MuiCreateTheme function, but couldnt get the style overrides working properly

---
+++ client/src/components/MenuDropdown/style.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+const MenuDropdownStyles = makeStyles((theme) => ({
+  accept: {
+    borderColor: 'green',
+    color: 'green'
+  },
+  buttons: {
+    textAlign: 'center'
+  },
+}));
+
+export default MenuDropdownStyles
---
+++ models/round.js
@@ -0,0 +1,35 @@
+const { model, Schema } = require('mongoose')
+
+const RoundSchema = new Schema({
+    course_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'course'
+    },
+    teebox: String,
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    members: [
+        {
+            user_id: {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            },
+            score: [
+                {
+                    hole_num: Number,
+                    score: Number
+                }
+            ]
+        }
+    ],
+    pending_members: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    ]
+})
+
+module.exports = model('round', RoundSchema)
