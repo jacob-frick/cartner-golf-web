289e28e2f673a183c118cf0937c9c6529d7e6359
Julian <Van Ost>
2020-04-03 19:46:53 -0700

    merged with Appbar styles and imports with Notifications branch

--- client/src/components/Appbar/Appbar.js
+++ client/src/components/Appbar/Appbar.js
@@ -1,13 +1,13 @@
 import React, { useContext, useState } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+// import Menu from '@material-ui/core/Menu';
+// import MenuItem from '@material-ui/core/MenuItem';
 import Drawer from '../Drawer'
 import DrawerContext from '../../utils/DrawerContext'
 import MenuDropdown from '../MenuDropdown';
