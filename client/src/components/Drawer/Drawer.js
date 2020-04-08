1ac9aef2a2f6d0d8c2016f4d80af91bfbbf42402
Julian <Van Ost>
2020-04-07 19:11:20 -0700

    changes logout icon to arrow-left, if that is desired; otherwise ignore this commit

--- client/src/components/Drawer/Drawer.js
+++ client/src/components/Drawer/Drawer.js
@@ -20,7 +20,8 @@ import Badge from '@material-ui/core/Badge';
 import Tooltip from '@material-ui/core/Tooltip';
 import DrawerContext from '../../utils/DrawerContext'
 import drawerStyles from './styles.js'
-import ExitToAppIcon from '@material-ui/icons/ExitToApp'
+// import ExitToAppIcon from '@material-ui/icons/ExitToApp'
+import ArrowBackOutlinedIcon from '@material-ui/icons/ArrowBackOutlined';
 import HomeIcon from '@material-ui/icons/Home'
 import User from '../../utils/User'
 //import HomeContext from './../../utils/HomeContext'
@@ -134,7 +135,7 @@ const DrawerComponent = () => {
               window.location.reload(false)
             }}>
             <ListItemIcon>
-              <ExitToAppIcon />
+              <ArrowBackOutlinedIcon />
             </ListItemIcon>
             <ListItemText primary="Log Out"
               aria-label='Log Out' />
